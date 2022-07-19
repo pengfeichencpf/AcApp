c3e9c704cabea7aeca59749297a5fcd074ac1131
pengfeichencpf <93218436+pengfeichencpf@users.noreply.github.com>
2022-07-19 16:29:52 -0700

    update to Acapp

--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-JS_PATH=/Users/pengfeichen/PycharmProjects/AcApp/game/static/js/
+JS_PATH=/home/acs/acapp/game/static/js/
 JS_PATH_DIST=${JS_PATH}dist/
-JS_PATH_SCR=${JS_PATH}src/
+JS_PATH_SRC=${JS_PATH}src/
 
-find $JS_PATH_SCR -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat | terser -c -m > ${JS_PATH_DIST}game.js
+
+echo yes | python3 manage.py collectstatic
